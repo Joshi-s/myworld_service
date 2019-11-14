1b3bc2700d4de06373a3d6ae4e4859ec0c787302
Joshi <shrinivas87.joshi@gmail.com>
2019-11-14 19:46:07 +0530

    Initial commit

---
+++ myworld_service/src/test/java/com/myworld/myworld_service/MyworldServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myworld.myworld_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyworldServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
